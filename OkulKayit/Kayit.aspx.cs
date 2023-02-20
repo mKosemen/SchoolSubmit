48187f9f6252dcaf737ffac0b642513f061e6696
mKosemen <103363649+mKosemen@users.noreply.github.com>
2023-02-20 17:56:03 +0300

    Add project files.

---
+++ OkulKayit/Kayit.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OkulKayit
+{
+    public partial class Kayit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            DataSetTableAdapters.ÖğrencilerTableAdapter dt= new DataSetTableAdapters.ÖğrencilerTableAdapter();
+            dt.OgrenciEkle(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text);
+        }
+    }
+}
